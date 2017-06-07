00f85bfbf43223ef45253a97bc39d4d98e43c028
tenpadennen <tenpadennen@yahoo.co.jp>
2017-06-07 18:46:39 +0900

    hogehoge

---
+++ sample001/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace sample001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("goodby hihi");
+
+        }
+    }
+}
